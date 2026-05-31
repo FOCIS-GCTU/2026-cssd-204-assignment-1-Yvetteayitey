1b4ffe14cf4624843a3ba3158211a1f1b528ade1
Yvetteayitey <2425403936@live.gctu.edu.gh>
2026-05-31 19:35:52 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number : 2
+#
+# Name: Yvette Yawa Ayitey
+# STUDENT ID: YYA2425403936
+# Email: 2425403936@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Yvette Yawa Ayitey, this programming assignment is my own work
+# and I have not provided this code from any other student.#
+
 
 def main():
-  pass #code goes here
-
+    # This function prints out my initials in a large stylized format
+    print()
+    print("...YYA")
+    print()
+    print("...YY........YY........YY........YY............AAAA.........")
+    print("....YY......YY..........YY......YY............AA..AA........")
+    print(".....YY....YY............YY....YY............AA....AA.......")
+    print("......YY..YY..............YY..YY............AA......AA......")
+    print(".......YYYY................YYYY.............AAAAAAAAAA......")
+    print(".......YYYY................YYYY.............AA......AA......")
+    print(".......YYYY................YYYY.............AA......AA......")
+    print(".......YYYY................YYYY.............AA......AA......")
+    print(".......YYYY.......**.......YYYY.......**....AA......AA....**")
+    print(".......YYYY.......**.......YYYY.......**....AA......AA....**")
 
 
 main()
